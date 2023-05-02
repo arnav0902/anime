4303062d6837fec3c70b8ebd782397f4c2c099ab
arnav0902 <108929578+arnav0902@users.noreply.github.com>
2023-05-02 18:54:15 +0530

    Add files via upload

---
+++ gojo.js
@@ -0,0 +1,37 @@
+img="";
+status="";
+objects=[];
+
+function preload(){
+img=loadImage('gojo.webp');
+}
+
+function setup(){
+canvas=createCanvas(640,450);
+canvas.center();
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+
+}
+function modelLoaded(){
+    console.log("modelLoaded!!!");
+    status=true;
+    objectDetector.detect(img,gotresults);
+
+    
+}
+
+function gotresults(error,results){
+    if (error){
+        console.log(error);
+
+    }
+  console.log(results);
+}
+function draw(){
+image(img,0,0,640,450);
+fill("#FF0000");
+text("Satoru Gojo",80,75);
+noFill();
+stroke("#FF0000");
+rect(80,60,450,500);
+}
